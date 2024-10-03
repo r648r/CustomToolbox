f2470cb722eb79ef7ef7965f4c69963e75a55770
r648r <raphael.lechappe@gmail.com>
2024-10-03 23:18:20 +0200

    :art: Hacker things

--- Dorker/modules/constants.py
+++ Dorker/modules/constants.py
@@ -1,6 +1,33 @@
+# modules/constants.py
+
 import random
 import os
 from colorama import Fore, Style
+
+
+#  ___   ____  ____   __    _     _    _____      _       __    _     _     ____ 
+# | | \ | |_  | |_   / /\  | | | | |    | |      \ \  /  / /\  | |   | | | | |_  
+# |_|_/ |_|__ |_|   /_/--\ \_\_/ |_|__  |_|       \_\/  /_/--\ |_|__ \_\_/ |_|__ 
+# 
+
+####################################
+# Test and add support with arguments + config file
+RESULTS_PER_PAGE = 10
+TOTAL_RESULTS = 100
+
+####################################
+
+OUTPUT_DIRECTORY_DEFAULT_PATH = "output"
+CONFIG_FILE_DEFAULT_PATH = ".dorkrc.yaml"
+LOG_DEFAULT_PATH=os.path.join("logs", "dorker.log")
+
+
+#   _____     __           
+#  / ___/__  / /__  _______
+# / /__/ _ \/ / _ \/ __(_-<
+# \___/\___/_/\___/_/ /___/
+#                          
+
 
 COLORS = {
     "RED": Fore.RED,
@@ -13,16 +40,11 @@ COLORS = {
     "WHITE": Fore.WHITE,
     "RESET": Style.RESET_ALL,
 }
-
-BANNER = """
+RANDOM_COLOR = random.choice(list(COLORS.values()))
+BANNER=r"""
     ___           __          
    / _ \___  ____/ /_____ ____
   / // / _ \/ __/  '_/ -_) __/
  /____/\___/_/ /_/\_\\__/_/        by D.Sanjai Kumar && r648r
 """
 
-RANDOM_COLOR = random.choice(list(COLORS.values()))
-RESULTS_PER_PAGE = 10
-TOTAL_RESULTS = 100
-CONFIG_FILE_DEFAULT_PATH = ".dorkrc.yaml"
-LOGGIN_DEFAULT_PATH=os.path.join("logs", "dorker.log")
