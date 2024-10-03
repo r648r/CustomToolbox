c6c753582d3bb4c41d49ecbfa39202adc044aa07
r648r <raphael.lechappe@gmail.com>
2024-10-03 23:13:18 +0200

    :building_construction: Remove arg parse in main()

--- Dorker/dorker.py
+++ Dorker/dorker.py
@@ -1,43 +1,48 @@
-#!/usr/bin/python3
-import argparse
+# dorker.py
+
 import logging
 import os
 
-# Import directory 'modules'
-from modules.constants import COLORS, RANDOM_COLOR, CONFIG_FILE_DEFAULT_PATH, LOGGIN_DEFAULT_PATH, BANNER
+# Import local
+from modules.constants import (
+    COLORS, RANDOM_COLOR, CONFIG_FILE_DEFAULT_PATH,
+    LOG_DEFAULT_PATH, BANNER, OUTPUT_DIRECTORY_DEFAULT_PATH
+)
 from modules.config_loader import load_config, get_config_values
-from modules.utils import validate_arguments
 from modules.dorking import handle_dorking
- 
-# Send much love to D.Sanjai Kumar : https://github.com/RevoltSecurities/GoogleDorker/blob/main/dorker/dorker.py
+from modules.arguments import parse_arguments  # Importer parse_arguments
 
 def main():
-    # Parse args 
-    parser = argparse.ArgumentParser(description=f"{COLORS['CYAN']}A Powerful Tool for Google Dorking{COLORS['RESET']}")
-    parser.add_argument("-q", "--query", help=f"[{COLORS['GREEN']}ALERT{COLORS['RESET']}]: Google dorking query for your target. Example: `-q inurl:test | inurl:env | inurl:dev | inurl:staging`", type=str)
-    parser.add_argument("-d", "--domain", help=f"[{COLORS['GREEN']}ALERT{COLORS['RESET']}]: Target name for Google dorking. Example: `-d example.com`", type=str)
-    parser.add_argument("-o", "--output", help=f"[{COLORS['GREEN']}ALERT{COLORS['RESET']}]: File name to save the dorking results. Example: `-o results.txt`", type=str)
-    args = parser.parse_args()
-    validate_arguments(args)
+    args = parse_arguments()
+    # -c défini > default vars in constants.py
+    if args.config:
+        config_file = args.config
+    else:
+        config_file = CONFIG_FILE_DEFAULT_PATH
+    config_data = load_config(config_file)
 
-    # Start search
+    # -o défini > config file value > default vars in constants.py
+    if args.output:
+        output_dir = args.output
+    else:
+        output_dir = get_config_values(config_data, 'output_directory', default=OUTPUT_DIRECTORY_DEFAULT_PATH)
+
     logging.info(f"{RANDOM_COLOR}{BANNER}{COLORS['RESET']}")
-    config_data = load_config(CONFIG_FILE_DEFAULT_PATH)
-    output_file = args.output if args.output else get_config_values(config_data, 'output_file', "dorker_results.txt")
-    handle_dorking(config_data, args.domain, output_file, query=args.query)
+    handle_dorking(config_data, args.domain, output_dir, query=args.query)
     logging.info(f"[{RANDOM_COLOR} Finish {COLORS['RESET']}]")
 
 if __name__ == "__main__":
     from modules.constants import COLORS
     logging.basicConfig(
-        format=f"[{COLORS['BLUE']}%(levelname)s{COLORS['RESET']}] [{COLORS['MAGENTA']}%(asctime)s{COLORS['RESET']}] %(message)s",
+        format=f"[{COLORS['BLUE']}%(levelname)s{COLORS['RESET']}] "
+               f"[{COLORS['MAGENTA']}%(asctime)s{COLORS['RESET']}] %(message)s",
         datefmt="%H:%M:%S",
         encoding='utf-8',
         level=logging.INFO,
         handlers=[
             logging.StreamHandler(),
-            logging.FileHandler(f"{LOGGIN_DEFAULT_PATH}", mode='a')
+            logging.FileHandler(f"{LOG_DEFAULT_PATH}", mode='a')
         ]
     )
-    
-main()
+
+    main()
