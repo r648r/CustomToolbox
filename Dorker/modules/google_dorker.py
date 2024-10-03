30e1c4daece538efa7793d5f78fd320155faa572
r648r <raphael.lechappe@gmail.com>
2024-10-03 23:21:37 +0200

    :goal_net: Catch error and fix bug

--- Dorker/modules/google_dorker.py
+++ Dorker/modules/google_dorker.py
@@ -2,6 +2,7 @@
 
 import logging
 import random
+import sys
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from icecream import ic
@@ -22,7 +23,7 @@ def build_google_service(api_keys):
         api_key = random.choice(api_keys)
         try:
             service = build('customsearch', 'v1', developerKey=api_key, cache_discovery=False)
-            return service, api_key
+            return service
         except HttpError as e:
             logging.error(f"Clé API invalide ou quota dépassé pour '{api_key}' - {e}")
             api_keys.remove(api_key)
@@ -40,7 +41,6 @@ def execute_search(service, cse_ids, search_query, results_per_page, total_resul
     while start_index < total_results:
         if not cse_ids:
             raise NoValidCSEIDException("Aucun ID CSE Google valide disponible.")
-            sys.exit(1)
         cse_id = random.choice(cse_ids)
         try:
             response = service.cse().list(
@@ -52,26 +52,30 @@ def execute_search(service, cse_ids, search_query, results_per_page, total_resul
             results.extend(response.get('items', []))
             start_index += results_per_page
         except HttpError as e:
-            logging.error(f"Échec de la recherche avec l'ID CSE '{cse_id}' - {e}")
+            status_code = e.resp.status
+            if status_code == 429:
+                logging.warning(f"RateLimitExceeded : ID CSE '{cse_id}' - HttpError {status_code}")
+            else:
+                logging.warning(f"Search failed with this CSE ID : '{cse_id}' - HttpError {status_code} - {e}")
             cse_ids.remove(cse_id)
             ic(type(cse_ids), cse_ids)
-            
         except Exception as e:
-            logging.error(f"Erreur inattendue lors de l'exécution de la recherche - {e}")
+            logging.error(f"Big problem bro - {e}")
             break
     return results
 
 
-def process_results(search_query, items):
+
+def process_results(items):
     """
     Traite et formate les résultats de la recherche.
-    Retourne une liste de chaînes de résultats formatées.
+    Retourne une liste de tuples contenant le titre et le lien.
     """
     results = []
     for item in items:
         title = item.get('title', 'Pas de titre')
         link = item.get('link', 'Pas de lien')
-        results.append(f"[{search_query}] [{link}] [{title}]")
+        results.append((title, link))
     return results
 
 def save_and_log_results(results, output_file, search_query):
@@ -79,24 +83,27 @@ def save_and_log_results(results, output_file, search_query):
     Enregistre et journalise les résultats dans le fichier de sortie.
     """
     if results:
-        for url in results:
-            logging.info(f"[{COLORS['GREEN']}TROUVÉ{COLORS['RESET']}] {url}")
-            write_result(output_file, url)
+        for title, link in results:
+            formatted_result = f"[{search_query}] [{link}] [{title}]"
+            logging.info(f"[{COLORS['GREEN']}FIND{COLORS['RESET']}] {formatted_result}")
+            write_result(output_file, search_query, link)
         logging.info(f"Total des URL trouvées pour '{search_query}': {len(results)}")
     else:
         logging.info(f"[{COLORS['RED']}Aucun résultat trouvé pour{COLORS['RESET']}] {search_query}")
 
 def query_google(search_query, google_api_keys, google_cse_ids, results_per_page, total_results, output_file):
-    """
-    Interroge le moteur de recherche personnalisé de Google en utilisant la requête fournie.
-    Retourne les listes mises à jour des clés API et des IDs CSE.
-    """
     try:
-        service, used_api_key = build_google_service(google_api_keys)
+        service = build_google_service(google_api_keys)
         raw_results = execute_search(service, google_cse_ids, search_query, results_per_page, total_results)
-        processed_results = process_results(search_query, raw_results)
+        processed_results = process_results(raw_results)
         save_and_log_results(processed_results, output_file, search_query)
         return google_api_keys, google_cse_ids
+    except NoValidAPIKeyException as e:
+        logging.error(f"{e}")
+        sys.exit(1)
+    except NoValidCSEIDException as e:
+        logging.error(f"{e}")
+        sys.exit(1)
     except Exception as e:
         logging.error(f"Échec de la fonction query_google pour '{search_query}' - {e}")
         return google_api_keys, google_cse_ids
